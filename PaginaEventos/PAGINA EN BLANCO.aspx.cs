572917a9fa7603fb571616efddfe828f5352f826
Lautaro-x <lautarohrodriguez@gmail.com>
2017-12-08 19:28:17 -0300

    8/12/17

--- PaginaEventos/PAGINA EN BLANCO.aspx.cs
+++ PaginaEventos/PAGINA EN BLANCO.aspx.cs
@@ -10,6 +10,6 @@ public partial class PAGINA_EN_BLANCO : System.Web.UI.Page
     Cabecera cabecera = new Cabecera();
     protected void Page_Load(object sender, EventArgs e)
     {
-        lbl_cabecera.Text = cabecera.GenerarCabecera("img/avatar2_small.jpg", "Lautaro", 1, 2, 3);
+        lbl_cabecera.Text = cabecera.GenerarCabecera("img/avatar2_small.jpg", "Lautaro", 2, 3);
     }
 }
