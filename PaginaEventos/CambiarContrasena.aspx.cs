00974da3eca6732dbeb8dbfe03b3dd5876cf9c04
Lautaro-x <lautarohrodriguez@gmail.com>
2017-12-08 20:04:25 -0300

    8/12/17 2

--- PaginaEventos/CambiarContrasena.aspx.cs
+++ PaginaEventos/CambiarContrasena.aspx.cs
@@ -7,8 +7,15 @@ using System.Web.UI.WebControls;
 
 public partial class CambiarContrasena : System.Web.UI.Page
 {
+    Cabecera cabecera = new Cabecera();
+    AccesoDatos ad = new AccesoDatos();
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        Session["CodigoCuenta"] = 1;
+        if (Session["CodigoCuenta"] == null) Response.Redirect("Pprincipal.aspx");
+        string imagen = ad.ObtenerValor("Select imagen from cuentas where codigo = " + Session["CodigoCuenta"]);
+        if (imagen == null) imagen = "img/avatar2_small.jpg";
+        string nombre = ad.ObtenerValor("Select nombre from cuentas where codigo = " + Session["CodigoCuenta"]);
+        lbl_cabecera.Text = cabecera.GenerarCabecera(imagen, nombre, 10, 20);
     }
 }
